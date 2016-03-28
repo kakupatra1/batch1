03dd9e2d8ab8779551644e027f557adf26070378
kakupatra1 <patrashaktiswarup@gmail.com>
2016-03-28 14:09:25 +0530

    Update ReflexionDemo.java
    Adding comments to the code base.

--- reflexion/ReflexionDemo.java
+++ reflexion/ReflexionDemo.java
@@ -4,8 +4,8 @@ public class ReflexionDemo {
 	public static void main(String[] args) throws Exception {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter Class Name");
-		String clssName = s.next();
-		Class clzz = Class.forName(clssName);
-		System.out.println(clzz.newInstance().getClass());
+		String clssName = s.next(); // string 'clssname' takes the entered class from console.
+		Class clzz = Class.forName(clssName); // forname () returns the 'clzz' object of class 'Class' with the given string name.
+		System.out.println(clzz.newInstance().getClass()); // returns the runtime class of object 'clzz' which is 'Class'.
 	}
 }
